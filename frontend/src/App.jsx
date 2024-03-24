ad26ac0fd93cba21fae5a67c4164a6b45bbf2aba
n33vb4k <bakshi.neev@gmail.com>
2024-03-24 17:08:10 +0000

    finished functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,56 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import ContactList from './ContactList'
-import ContactForm from './ContactForm'
+import { useState, useEffect } from "react";
+import ContactList from "./ContactList";
+import "./App.css";
+import ContactForm from "./ContactForm";
 
 function App() {
-  const [contacts, setContacts] = useState([{"firstName": "Neev", "lastName": "Bakshi", "email": "bakshi.neev@gmail.com", id: 1}])
+  const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentContact, setCurrentContact] = useState({})
 
-  //calls fetch contacts as soon as this component renders
   useEffect(() => {
     fetchContacts()
-  }, [])
+  }, []);
 
-  //sends request to backend to get contacts, waits for response and stores
   const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5000/contacts")
-    const data = await response.json()
-    setContacts(data.contacts)
-    console.log(data.contacts)
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setCurrentContact({})
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
+  const openEditModal = (contact) => {
+    if (isModalOpen) return
+    setCurrentContact(contact)
+    setIsModalOpen(true)
+  }
+
+  const onUpdate = () => {
+    closeModal()
+    fetchContacts()
   }
 
   return (
     <>
-    <ContactList contacts={contacts}/>
-    <ContactForm />
+      <ContactList contacts={contacts} updateContact={openEditModal} updateCallback={onUpdate} />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={closeModal}>&times;</span>
+          <ContactForm existingContact={currentContact} updateCallback={onUpdate} />
+        </div>
+      </div>
+      }
     </>
   );
-  
 }
 
-export default App
+export default App;
--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
 
-const ContactForm = ({}) => {
-    //set up variables
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [email, setEmail] = useState("")
+const ContactForm = ({ existingContact = {}, updateCallback }) => {
+    const [firstName, setFirstName] = useState(existingContact.firstName || "");
+    const [lastName, setLastName] = useState(existingContact.lastName || "");
+    const [email, setEmail] = useState(existingContact.email || "");
 
-    //what happens when a form is submitted
+    const updating = Object.entries(existingContact).length !== 0
+
     const onSubmit = async (e) => {
         e.preventDefault()
+
         const data = {
             firstName,
             lastName,
             email
         }
-        const url = "http://127.0.0.1:5000/create_contact"
+        const url = "http://127.0.0.1:5000/" + (updating ? `update_contact/${existingContact.id}` : "create_contact")
         const options = {
-            method: "POST",
+            method: updating ? "PATCH" : "POST",
             headers: {
                 "Content-Type": "application/json"
             },
@@ -26,42 +27,41 @@ const ContactForm = ({}) => {
         if (response.status !== 201 && response.status !== 200) {
             const data = await response.json()
             alert(data.message)
-        }
-        else {
-            //successfull
+        } else {
+            updateCallback()
         }
     }
 
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div>
                 <label htmlFor="firstName">First Name:</label>
-                <input 
+                <input
                     type="text"
                     id="firstName"
                     value={firstName}
                     onChange={(e) => setFirstName(e.target.value)}
-                    />
+                />
             </div>
             <div>
                 <label htmlFor="lastName">Last Name:</label>
-                <input 
+                <input
                     type="text"
-                    id="firstName"
+                    id="lastName"
                     value={lastName}
                     onChange={(e) => setLastName(e.target.value)}
-                    />
+                />
             </div>
             <div>
                 <label htmlFor="email">Email:</label>
-                <input 
+                <input
                     type="text"
                     id="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    />
+                />
             </div>
-            <button type = "submit">Create Contact</button>
+            <button type="submit">{updating ? "Update" : "Create"}</button>
         </form>
     );
 };
